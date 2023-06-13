c2fc659a38429a931ae6dc3697abffa6aa7d9180
Alexandre Moreira <alexandre.rocha@estudante.ufjf.br>
2023-06-13 19:40:52 -0300

    add auxiliaries functions

---
+++ src/components/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60, 
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",
+    },
+    information: {
+        fontSize: 25,
+        color: "#038cfc",
+        fontWeight: "bold",
+    },
+    numberimc: {
+        fontSize: 60,
+        color: "#038cfc",
+        fontWeight: "bold",
+    }
+});
+
+export default styles
